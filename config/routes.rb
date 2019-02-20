b28689a6b528c0358fe3c78a21f890a8a19909c4
山藤大 <masaru_0511_496@yahoo.co.jp>
2019-02-20 15:37:31 +0900

    編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
   root 'items#index'
   devise_for :users, controllers: { sessions: 'sessions' ,registrations: "registrations"}
-<<<<<<< HEAD
-  resources :items, except: [:edit, :destroy] do
-    resources :comments, only: [:create]
-=======
   resources :items, except: [:edit, :destroy] do 
      resources :likes, only: [:create, :destroy]
->>>>>>> origin/master
   end
   resources :users do
     collection do
